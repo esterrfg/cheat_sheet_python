18539c089299f2c2d07f1b69b072bc278a248f7b
Ester Figini Gerling <esterfigini@gmail.com>
2025-08-29 16:30:14 -0300

    adicionado data visualization e data treatment

--- cheat_sheet_guidebook_ester.py
+++ cheat_sheet_guidebook_ester.py
@@ -46,10 +46,54 @@ def write_json(data, file_path):
 df_normalizada = pd.json_normalize(df)
 
 ###############################################################
-#-----------------Sneak peak on type of data------------------#
+#-----------------------Data Treatment------------------------#
 ###############################################################
 for col in df.columns:
     print(f"Coluna: {col}")
     print(df[col].unique())
     print("-" * 30)
+
+df.duplicated().sum() # numero de entradas duplicadas
+
+df.isna().sum() # numero de entradas vazias por coluna
+df.isna().sum().sum() # numero de entradas vazias total
+
+df.describe() # estatisticas descritivas
+
+###############################################################
+#-------------------Data Visualization------------------------#
+###############################################################
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# boxplot para visualizar outliers
+sns.boxplot(x=df['coluna_numerica'])
+
+# quartis
+Q1 = df['coluna_numerica'].quantile(.25)
+Q3 = df['coluna_numerica'].quantile(.75)
+IQR = Q3-Q1
+limite_inferior = Q1 - 1.5*IQR
+limite_superior = Q3 + 1.5*IQR
+
+outliers_index = (df['coluna_numerica'] < limite_inferior) | (df['coluna_numerica'] > limite_superior)
+df[outliers_index]['coluna_numerica']
+df_sem_outliers = df[~outliers_index]
+
+# Visualizando a distribuição das variáveis numéricas
+sns.set(style="whitegrid")
+plt.figure(figsize=(12, 6))
+sns.histplot(df['coluna_numerica'], bins=30, kde=True)
+plt.title('Distribuição da Coluna Numérica')
+plt.xlabel('Valores')
+plt.ylabel('Frequência')
+plt.show()
+
+# Visualizando a relação entre duas variáveis
+plt.figure(figsize=(12, 6))
+sns.scatterplot(x='coluna_x', y='coluna_y', data=df)
+plt.title('Relação entre Coluna X e Coluna Y')
+plt.xlabel('Coluna X')
+plt.ylabel('Coluna Y')
+plt.show()
 
